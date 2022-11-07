7791edf344e49df8141056e1afc6dff58531ea25
Ajwad <aibnswalehin@gmail.com>
2022-11-07 21:10:52 +0000

    Embeds
    - Added embeds to commands
    - Moved syncing to the on_ready function

--- main.py
+++ main.py
@@ -22,24 +22,22 @@ def find_time(hours, minutes):
 @bot.event
 async def on_ready():
     await bot.change_presence(activity = discord.Game("Reminding since 2022!"))
+    await bot.tree.sync(guild=None)
     print(f"We have logged in as Remind Me!")
-
-
-# Manually syncing the bot to fit with commands
-@bot.command()
-async def sync(ctx):
-    await ctx.send(f"Syncing...")
-    await bot.tree.sync()
-    await bot.tree.sync(guild = discord.Object(id=000000000000000000000000))
-    await ctx.send(f"Syncing Complete!")
-
 
 # Remind function to remind after a given amount of time
 @bot.command()
 async def remind(ctx, message:str, hour:int, minute: int, second:int):
     wait = time_conversion(hour, minute, second)
     await asyncio.sleep(wait)
-    await ctx.send(f"{ctx.author.mention}, Reminder: {message}")
+
+    remindembed = discord.Embed(
+        colour = discord.Colour.yellow()
+    )
+    remindembed.set_author(name = "Reminder:", icon_url = "https://i.pinimg.com/originals/5e/a2/fc/5ea2fc59e9d958e0eac64909f38eabc4.jpg")
+    remindembed.add_field(name = f"Reminder from {wait} seconds ago:", value = f"{message}")
+    
+    await ctx.send(ctx.author.mention, embed = remindembed)
 
 
 # Remind function to remind after a given time
@@ -48,26 +46,14 @@ async def timeremind(ctx, hours, minutes, daily_reminder):
     now, then = find_time(hours, minutes) 
     wait_time = (then-now).total_seconds()
     await asyncio.sleep(wait_time)
-    await ctx.send(daily_reminder)
 
+    remindembed = discord.Embed(
+        colour = discord.Colour.yellow()
+    )
+    remindembed.set_author(name = "Reminder:", icon_url = "https://i.pinimg.com/originals/5e/a2/fc/5ea2fc59e9d958e0eac64909f38eabc4.jpg")
+    remindembed.add_field(name = f"Reminder from {wait_time} seconds ago", value = f"{daily_reminder}")
 
-# Embed command: shows what the bot does
-@bot.command()
-async def embed(ctx, member:discord.Member = None):
-    if member == None:
-        member = ctx.author
-
-    name = member.display_name
-    pfp = member.display_avatar
-
-    embed = discord.Embed(title = "Remind Me!", description="Commands:", colour = discord.Colour.random())
-    embed.set_author(name = f"{name}", icon_url = "https://i.pinimg.com/originals/5e/a2/fc/5ea2fc59e9d958e0eac64909f38eabc4.jpg")
-    embed.set_thumbnail(url=f"{pfp}")
-    embed.add_field(name = "!remind", value = "Sets a reminder after a given amount of time")
-    embed.add_field(name = "!timeremind", value = "Sets a reminder at a given time")
-    embed.set_footer(text=f"{name} Made this Embed")
-    await ctx.send(embed = embed)
-
+    await ctx.send(ctx.author.mention, embed = remindembed)
 
 # Sets the bot token (Not showing because it allows people to have access to the bot)
 bot.run("Bot Token")
